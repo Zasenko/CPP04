8a7ddaea72aeeec7e88c45b2348f2dabaa52a8e7
Dmitry Zasenko <dzasenko@c3r7p6.42vienna.com>
2025-07-24 11:59:04 +0200

    ex00 add Wrong classes

---
+++ ex00/WrongCat.cpp
@@ -0,0 +1,32 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() : WrongAnimal()
+{
+    std::cout << "WrongCat default constructor called" <<std::endl;
+
+    type = "WrongCat";
+}
+
+WrongCat::WrongCat(const WrongCat &copy): WrongAnimal(copy)
+{
+    std::cout << "WrongCat copy constructor called" <<std::endl;
+}
+
+WrongCat::~WrongCat()
+{
+    std::cout << "WrongCat destructor called" <<std::endl;
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &src)
+{
+    std::cout << "WrongCat assignment operator called" <<std::endl;
+    if (this != &src) {
+        WrongAnimal::operator=(src);
+    }
+    return *this;
+}
+
+void WrongCat::makeSound() const
+{
+    std::cout << "WrongCat Miiiaaaaauuuuuuu....." <<std::endl;
+}
