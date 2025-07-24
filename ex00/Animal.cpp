b44b46e86cad771ab37d05daa461d738456ce944
Dmitry Zasenko <dzasenko@c3r7p6.42vienna.com>
2025-07-24 10:24:26 +0200

    create simple classes

---
+++ ex00/Animal.cpp
@@ -0,0 +1,31 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+
+}
+
+Animal::Animal(const std::string &type)
+{
+
+}
+
+Animal::Animal(const Animal &copy)
+{
+
+}
+
+Animal::~Animal()
+{
+
+}
+
+Animal &Animal::operator=(const Animal &src)
+{
+
+}
+
+void makeSound()
+{
+
+}
